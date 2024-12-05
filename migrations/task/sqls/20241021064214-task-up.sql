50aa0e5335a0c35fd6efb8d57b5f6afec363ad0b
Peter <dayr88520@gmail.com>
2024-12-06 00:51:36 +0800

    feat: TASK 6 FINISHED

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -315,14 +315,62 @@ INNER JOIN (
 -- 6-1 查詢：查詢專長為重訓的教練，並按經驗年數排序，由資深到資淺（需使用 inner join 與 order by 語法)
 -- 顯示須包含以下欄位： 教練名稱 , 經驗年數, 專長名稱
 
+SELECT
+	u."name" AS 教練名稱,
+	c.experience_years AS 經驗年數,
+	s."name" AS 專長名稱
+FROM
+	"COACH_LINK_SKILL" cls
+INNER JOIN "SKILL" s ON cls.skill_id = s.id
+INNER JOIN "COACH" c ON cls.coach_id = c.id
+INNER JOIN "USER" u ON c.user_id = u.id
+WHERE s."name" = '重訓'
+ORDER BY c.experience_years ASC;
+
 -- 6-2 查詢：查詢每種專長的教練數量，並只列出教練數量最多的專長（需使用 group by, inner join 與 order by 與 limit 語法）
 -- 顯示須包含以下欄位： 專長名稱, coach_total
+
+SELECT
+	s."name" AS 專長名稱,
+	COUNT(*) AS coach_total
+FROM
+	"COACH_LINK_SKILL" cls
+INNER JOIN "SKILL" s ON cls.skill_id = s.id
+INNER JOIN "COACH" c ON cls.coach_id = c.id 
+INNER JOIN "USER" u ON c.user_id = u.id
+WHERE u."role" = 'COACH' -- 與COACH表關聯並確認是否為教練身分
+GROUP BY s."name" -- 
+ORDER BY coach_total DESC -- 遞減排序
+LIMIT 1;
 
 -- 6-3. 查詢：計算 11 月份組合包方案的銷售數量
 -- 顯示須包含以下欄位： 組合包方案名稱, 銷售數量
 
+SELECT
+	cp."name" AS 組合包方案名稱,
+	COUNT(*) AS 銷售數量
+FROM
+	"CREDIT_PURCHASE" cpc
+INNER JOIN "CREDIT_PACKAGE" cp ON cpc.credit_package_id = cp.id
+WHERE cpc.created_at BETWEEN '2024-12-01' AND '2024-12-31'
+GROUP BY cp."name";
+
 -- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
 -- 顯示須包含以下欄位： 總營收
 
+SELECT
+	SUM(price_paid) AS 總營收
+FROM
+	"CREDIT_PURCHASE"
+WHERE purchase_at BETWEEN '2024-12-01' AND '2024-12-31';
+
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
+
+SELECT  
+	COUNT(DISTINCT(user_id)) AS 預約會員人數 -- 排除重複項 並計算總數
+FROM
+	"COURSE_BOOKING"
+WHERE status != '課程已取消'
+AND
+booking_at BETWEEN '2024-11-01' AND '2024-11-30';
